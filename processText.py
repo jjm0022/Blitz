20737281092dc3ce267af011a4b43bb22220304a
JJ <jj.miller.jm@gmail.com>
2019-06-21 14:06:48 -0400

    Create a processing pipeline class

--- processText.py
+++ processText.py
@@ -1,5 +1,8 @@
 import spacy
+from forecastDb import DB
 
+from dateutil.parser import parse
+import re
 
 
 '''
@@ -8,57 +11,76 @@ TODO Different tables for POS, ENTITY, PHRASES, TERM_COUNTS
       - Each forecast should be processed before
 '''
 
-class Clean():
+class Pipeline(object):
 
-  def __init__(self):
+  def __init__(self, row, preprocess=True):
     '''
+    Row is a dict of a row from the AFD table
     '''
-    self.nlp = spacy.load('en_core_web_sm')
+    self._nlp = spacy.load('en_core_web_sm')
+    self.forecast = row['Forecast']
+    self.raw_forecast = row['Forecast']j
+    self.time_string = row['TimeStamp']
+    self.time_ = parse(row['TomeStamp'])
+    self.office = row['Office']
     
-  def tokenize(self, text):
+    if preprocess:
+      self._preProcess()
+    
+
+  def _preProcess(self):
     '''
+    Processes the text during initialization
     '''
-    clean_sections = list()
+    self.forecast = self.processText(self.forecast)
+
+  def connect(self, db_path):
+    '''
+    Connect to database
+    '''
+    self.db = DB(dp_path)
+
+  def processText(self, text):
+    '''
+    Cleans text
+    Input: text <string>
+    Returns: <string> with irrelevant characters removed    
+    '''
+    clean_text = ''
     sections = text.split('\n\n')
+      
     for sec in sections:
       if len(sec.strip()) > 25:
-        clean_sections.append(sec.strip().replace('\n', ' '))
-  
-  def clean(self, text):
+        clean_text = clean_text + sec.strip() + ' '
+    
+    # Remove new-line characters
+    clean_text = re.sub(r'\n', ' ', clean_text)
+    # Remove everything but letters and punctuation
+    clean_text = re.sub(r'[^a-zA-Z0-9\s\.\,\!\?\']', ' ', clean_text)
+    # Remove ...
+    clean_text = re.sub(r'\.{2,}', ' ', clean_text)
+    # Remove any areas with more than one white-space back to back
+    clean_text = re.sub(r'\s{2,}', ' ', clean_text)
+    clean_text = re.sub(r'\s\.', '.', clean_text)
+    clean_text = re.sub(r'\.{2,}', '.', clean_text)
+    
+    self.forecast = text
+
+  def filterTrash(doc, numbers=True, stopwords=True):
     '''
+    Returns true if token is not a word or number
     '''
-    doc = self.tokenizer(text)
-    for sent in doc.sents:
-      if len(sent.text) > 20:
-        
-    return sec
-  
-  def removeTrash(doc):
-    '''
-    '''
-    if doc.is_digit:
-      return False
-    elif doc.is_digit:
-      return False
-    elif doc.is_punct:
-      return False
-    elif doc.is_space:
-      return False
-    elif doc.is_bracket:
-      return False
-    elif doc.is_quote:
-      return False
-    elif doc.like_url:
-      return False
-    elif doc.like_email:
-      return False
-    elif doc.like_num:
-      return False
-    elif doc.is_stop:
-      return False
+    if numbers:
+      if doc.like_num:
+        return False
+    if stopwords:
+      if doc.is_stop:
+        return False
+
+    if (doc.is_punct or doc.is_space or doc.is_bracket or doc.is_quote or 
+        doc.like_url or doc.like_email):
+       return False
     else:
       return True
-
-
 
   
