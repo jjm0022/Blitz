f73a7e267b14de4f641a450720aafad333dd173b
JJ <jj.miller.jm@gmail.com>
2019-09-22 21:40:24 -0400

    Start making changes to incorporate SQLAlchemy into workflow

--- database.py
+++ database.py
@@ -3,6 +3,22 @@ import re
 from datetime import datetime
 import os
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from database_definitions import Forecast, Status, Extraction, Office
+
+class DB:
+
+    def __init__(self,
+                 url='mysql+mysqldb://root:b14z3r5@10.0.0.100:3306/testdb',
+                 engine=None,
+                 echo=False):
+        if engine:
+            self.session = Session(bind=engine)
+        else:
+            engine = create_engine(url, echo=False)
+            self.session = Session(bind=engine)
 
 class DB(object):
 
