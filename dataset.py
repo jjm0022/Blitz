8caa64ce6d41c55c18e5bfc67dd2e4123a9f158e
JJ <jj.miller.jm@gmail.com>
2019-10-08 16:29:23 -0400

    Make adjustments to work with SQLAlchemy

--- dataset.py
+++ dataset.py
@@ -5,110 +5,47 @@ import os
 
 from database import DB
 from extract import Pipeline
+from database_definitions import Extraction, Office, Forecast, Status
 
 
-class Connection(DB):
+class Connection:
 
     def __init__(self):
         self.db = DB()
+        self.session = self.db.session
 
-    def notInDataset(self):
-        '''
-        '''
-        connection = self.db.connection
-        with connection as c:
-            cur = c.cursor()
-            query = cur.execute('''SELECT * FROM Forecast f WHERE uID
-                                   IN(SELECT uID FROM Processed WHERE Dataset = 0)
-                                   ORDER BY Office, TimeStamp;''')
-            rows = query.fetchall()
-        for row in rows:
-            yield row
+    @property
+    def total_not_in_dataset(self):
+        return self.session.query(Status).\
+            filter(Status.in_dataset == 0).\
+            count()
 
-    def updateEntry(self, uID):
-        '''
-        '''
-        query = '''UPDATE Processed
-                    SET Dataset=1
-                    WHERE uID=?;'''
-        connection = self.db.connection
-        with connection as c:
-            cur = c.cursor()
-            cur.execute(query, (uID,))
+    @property
+    def total_in_dataset(self):
+        return self.session.query(Status).\
+            filter(Status.in_dataset == 1).\
+            count()
 
-    def getProcessedPhrases(self, uID):
+    def get_not_in_dataset(self, limit=1000):
         '''
-        Returns rows matching the uID from the Phrase table
         '''
-        connection = self.db.connection
-        with connection as c:
-            cur = c.cursor()
-            query = cur.execute('''SELECT * FROM Phrase WHERE uID=?''', (uID,))
-            rows = query.fetchall()
-        for row in rows:
-            yield row
+        unprocessed = self.session.query(Status).filter(
+            Status.in_dataset == 0).limit(limit)
+        print(f"Found {len(unprocessed)} forecasts to be added to the dataset")
+        for status in unprocessed:
+            yield status
+    
+    def get_forecast_extractions(self, forecast):
+        '''
+        '''
+        extractions = self.session.query(Extraction).filter(
+            Extraction.forecast_id == forecast.id).all()
+        print(f"Found {len(extractions)} extractions for {forecast.id}")
+        for extraction in extractions:
+            yield extraction 
 
 
 class Dataset(Connection):
-  '''
-  This class deals with generating a dataset for training a entity recognition model.
-  The phrases are being labeled with a generic label for right now. 
-  '''
-
-  def __init__(self, dataset_path=None):
-    Connection.__init__(self)    
-
-    self.Annotation = collections.namedtuple('Annotation', ['start', 'end', 'label', 'content', 'id', 'phrase'])
-
-    if dataset_path:
-      self.dataset_path = dataset_path
-    else:
-      self.dataset_path = os.path.join('./data', 'nws_phrase_dataest.jsonl')
-
-  def add2Dataset(self, total=1e35):
-    '''
-    '''
-    # Grab the rows that have not been added to the dataset
-    for iteration, forecast in enumerate(self.notInDataset()):
-      if iteration >= total:
-        break
-      self.json_lines = list()
-      print(f"Processing phrases from {forecast['uID']}")
-      #print(f"Processing phrases for {forecast['Office']} on {forecast['TimeStamp']}")
-      pipe = Pipeline(forecast)
-      self.annotations = list()
-      annotations_list = list()
-
-      # for each forecast, store it, and the annotations in a single json with 
-      for row in self.db.getProcessedPhrases(forecast['uID']):
-        annotation = self.Annotation(start=row['StartIndex'],
-                                end=row['EndIndex'],
-                                label='PlaceHolder',
-                                content=forecast['Forecast'],
-                                phrase=row['Phrase'],
-                                id=row['uID'])
-          
-        if self._AddAnnotation(annotation):
-          annotations_list.append(self._AnnotationToJson(annotation))
-        
-        self.updateEntry(annotation.id)
-      if len(annotations_list) == 0:
-        continue
-      print(f'Adding {len(annotations_list)} annotations')
-      self.json_lines.append(self._toJSON(annotations_list, pipe))
-
-      with open('nws_phrase_dataset.jonsl', 'a+') as t:
-        t.writelines(self.json_lines)
-
-  def _AddAnnotation(self, annotation):
-    for a in self.annotations:
-      if self._HasOverlap(annotation, a):
-        print(f'Overlap with {a.phrase} and {annotation.phrase}')
-        return False
-    self.annotations.append(annotation)
-    return True
-
-  def _HasOverlap(self, a1, a2):
     '''
     This class deals with generating a dataset for training a entity recognition model.
     The phrases are being labeled with a generic label for right now.
@@ -117,7 +54,8 @@ class Dataset(Connection):
     def __init__(self, dataset_path=None):
         Connection.__init__(self)
 
-        self.Annotation = collections.namedtuple('Annotation', ['start', 'end', 'label', 'content', 'id', 'phrase'])
+        self.Annotation = collections.namedtuple(
+            'Annotation', ['start', 'end', 'label', 'content', 'id', 'phrase'])
 
         if dataset_path:
             self.dataset_path = dataset_path
@@ -125,39 +63,45 @@ class Dataset(Connection):
             project_path = os.path.join(os.environ['GIT_HOME'], 'AFDTools')
             self.dataset_path = os.path.join(project_path, 'data', 'nws_phrase_dataest.jsonl')
 
-    def add2Dataset(self, total=1e35):
+    def add2Dataset(self, limit=1000):
         '''
         '''
         # Grab the rows that have not been added to the dataset
-        for iteration, forecast in enumerate(self.notInDataset()):
-            if iteration >= total:
-                break
+        for status in self.get_not_in_dataset(limit=limit):
+            forecast = status.forecast
+            office = forecast.office
             self.json_lines = list()
-            print(f"Processing phrases from {forecast['uID']}")
+            print(f"Processing phrases from {forecast.id}")
             pipe = Pipeline(forecast)
             self.annotations = list()
             annotations_list = list()
 
-            # for each forecast, store it, and the annotations in a single json with
-            for row in self.getProcessedPhrases(forecast['uID']):
-                annotation = self.Annotation(start=row['StartIndex'],
-                                             end=row['EndIndex'],
-                                             label='PlaceHolder',
-                                             content=forecast['Forecast'],
-                                             phrase=row['Phrase'],
-                                             id=row['uID'])
+            try:
+                # for each forecast, store it, and the annotations in a single json with
+                for extraction in self.get_forecast_extractions(forecast):
+                    annotation = self.Annotation(start=extraction.start_index,
+                                                end=extraction.end_index,
+                                                label='PlaceHolder',
+                                                phrase=extraction.phrase,
+                                                id=extraction.forecast_id)
 
-                if self._AddAnnotation(annotation):
-                    annotations_list.append(self._AnnotationToJson(annotation))
+                    if self._AddAnnotation(annotation):
+                        annotations_list.append(self._AnnotationToJson(annotation))
 
-                self.updateEntry(annotation.id)
-            if len(annotations_list) == 0:
-                continue
-            print(f'Adding {len(annotations_list)} annotations')
-            self.json_lines.append(self._toJSON(annotations_list, pipe))
+                    status.in_dataset = 1
+                    self.session.flush()
 
-            with open(self.dataset_path, 'a+') as t:
-                t.writelines(self.json_lines)
+                if len(annotations_list) == 0:
+                    continue
+                print(f'Adding {len(annotations_list)} annotations')
+                self.json_lines.append(self._toJSON(annotations_list, pipe))
+
+                with open(self.dataset_path, 'a+') as t:
+                    t.writelines(self.json_lines)
+            except Exception as e:
+                print(e)
+                self.session.commit()
+
 
     def _AddAnnotation(self, annotation):
         for a in self.annotations:
@@ -197,3 +141,8 @@ class Dataset(Connection):
             },
         }
         return json.dumps(json_obj, ensure_ascii=False) + '\n'
+
+
+if __name__ == '__main__':
+    d = Dataset()
+    d.add2Dataset(limit=10)
