3f0969b75bd1abf9040aaa9a11c00484b8e763e0
jjm0022 <jj.miller.jm@gmail.com>
2020-03-02 21:13:00 -0500

    If config.ini doesn't exist, generate a sqlite db (only for circle ci)

--- database.py
+++ database.py
@@ -1,26 +1,46 @@
 import configparser
+import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from sqlalchemy.schema import MetaData
+from pathlib import Path
 
 from database_definitions import Forecast, Status, Extraction, Office
 
 
 class DB:
-    def __init__(
-        self,
-        url=None,
-        engine=None,
-        echo=False,
-    ):
+    def __init__(self, url=None, engine=None, echo=False, config_path="config.ini"):
         if not url:
             config = configparser.ConfigParser()
-            config.read('config.ini')
-            url = f"mysql+mysqldb://{config['db']['user']}:{config['db']['passwd']}@{config['db']['host']}:{config['db']['port']}/{config['db']['schema']}"
+            if Path(config_path).exists():
+                config.read()
+                url = f"mysql+mysqldb://{config['db']['user']}:{config['db']['passwd']}@{config['db']['host']}:{config['db']['port']}/{config['db']['schema']}"
+            else:
+                # Hopefully this means we're building in circleCI
+                url = "sqlite:///ci_build.db"
+
         if engine:
-            self.session = Session(bind=engine)
+            self.engine = engine
+            self.session = Session(bind=self.engine)
         else:
-            engine = create_engine(url, echo=False)
+            self.engine = create_engine(url, echo=False)
             self.session = Session(bind=engine)
 
-    def _reset_databse():
-        pass
+        self.create_tables([Forecast, Office, Status, Extraction])
+
+    def create_tables(self, tables):
+        """
+        Description:
+            Creates a table in connected DB if it does not already exist
+
+        Arguments:
+            tables {list} -- list of pre-defined SQLAlchemy tables
+        """
+        for t in tables:
+            assert isinstance(
+                t, sqlalchemy.ext.declarative.api.DeclarativeMeta
+                ), f"'{t}' must be an instance of sqlalchemy.ext.declarative.api.DeclarativeMeta"
+
+            print(f"Creating {t.__tablename__} if it does not exist")
+            t.__table__.create(bind=self.engine, checkfirst=True)
+            
